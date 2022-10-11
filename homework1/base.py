90103fbe1f354b8199eb40478e0b6113acff7281
Danil Vorobev <dvorobiev@agentapp.ru>
2022-10-11 16:38:58 +0300

    Были написаны тестовые сценарии к homework1 (test.py), часть логики была вынесена в base.py Используемые локаторы находятся в ui/locators/basic_locators.py Фикстуры находятся в conftest.py

---
+++ homework1/base.py
@@ -0,0 +1,66 @@
+import pytest
+from ui.locators import basic_locators
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException, ElementClickInterceptedException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+CLICK_RETRY = 3
+
+
+class BaseCase:
+    driver = None
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, driver):
+        self.driver = driver
+
+    def wait(self, timeout=None):
+        if timeout is None:
+            timeout = 15
+        return WebDriverWait(self.driver, timeout=timeout)
+
+    def find(self, locator, timeout=None):
+        return self.wait(timeout).until(EC.visibility_of_element_located(locator))
+
+    def is_present(self, locator):
+        try:
+            self.find(locator)
+            return True
+        except NoSuchElementException:
+            return False
+
+    def click_button(self, locator):
+        for i in range(CLICK_RETRY):
+            try:
+                button = self.find(locator)
+                button.click()
+                return
+            except NoSuchElementException:
+                if i == CLICK_RETRY - 1:
+                    raise NoSuchElementException('Button was not found')
+            except ElementClickInterceptedException:
+                if i == CLICK_RETRY - 1:
+                    raise ElementClickInterceptedException('Cant click the button')
+
+    def login(self, login, password):
+        self.click_button(basic_locators.HEADER_LOGIN_BUTTON)
+        email = self.find(basic_locators.EMAIL_FIELD)
+        email.clear()
+        email.send_keys(login)
+        password_field = self.find(basic_locators.PASSWORD_FIELD)
+        password_field.clear()
+        password_field.send_keys(password)
+        self.find(basic_locators.WRAP_LOGIN_BUTTON).click()
+
+    def logout_click(self):
+        action = ActionChains(self.driver)
+        header = self.find(basic_locators.HEADER_BUTTON)
+        logout = self.find(basic_locators.LOGOUT_BUTTON)
+        action.click(header).pause(1).click(logout).perform()
+
+    def change_profile_field(self, locator, new_value):
+        field = self.find(locator)
+        field.clear()
+        field.send_keys(new_value)
---
+++ homework1/test.py
@@ -0,0 +1,61 @@
+import pytest
+import random
+
+from base import BaseCase
+from ui.locators import basic_locators
+
+
+class TestOne(BaseCase):
+    login_value = 'hsabuse@yandex.ru'
+    pass_value = 'Qwe123-'
+
+    @pytest.mark.UI
+    def test_login(self):
+        self.login(login=self.login_value, password=self.pass_value)
+        assert self.find(basic_locators.USERNAME).text == self.login_value.upper()
+
+    @pytest.mark.UI
+    def test_logout(self):
+        self.login(login=self.login_value, password=self.pass_value)
+        assert self.find(basic_locators.USERNAME).text == self.login_value.upper()
+        self.driver.refresh()
+        self.click_button(basic_locators.HEADER_BUTTON)
+        self.click_button(basic_locators.LOGOUT_BUTTON)
+        assert self.is_present(basic_locators.MAIN_PAGE_DIV) == True
+        assert self.driver.current_url == 'https://target-sandbox.my.com/'
+
+    @pytest.mark.UI
+    def test_negative_invalid_login_or_pass(self):
+        self.login(login='111', password='111')
+        assert self.is_present(basic_locators.LOGIN_FORM_MESSAGE_TITLE) == True
+        assert self.is_present(basic_locators.LOGIN_FORM_MESSAGE_TEXT) == True
+        assert self.find(basic_locators.LOGIN_FORM_MESSAGE_TITLE).text == 'Error'
+        assert self.find(basic_locators.LOGIN_FORM_MESSAGE_TEXT).text == 'Invalid login or password'
+
+    @pytest.mark.UI
+    def test_negative_login_with_russian_credentials(self):
+        self.login(login='почта', password='пароль')
+        assert self.is_present(basic_locators.ENTER_EMAIL_OR_PHONE_ERROR) == True
+
+    @pytest.mark.UI
+    def test_change_profile_data(self):
+        self.login(login=self.login_value, password=self.pass_value)
+        self.click_button(basic_locators.PROFILE_BUTTON)
+        new_inn = str(random.randint(1000000000, 9999999999))
+        self.change_profile_field(basic_locators.INN_FIELD, new_inn)
+        new_phone = ('+7999' + str(random.randint(1000000, 9999999)))
+        self.change_profile_field(basic_locators.PHONE_FIELD, new_phone)
+        self.click_button(basic_locators.SAVE_PROFILE_BUTTON)
+        assert self.is_present(basic_locators.INFORMATION_CHANGED_MESSAGE) == True
+        info = self.find(basic_locators.INFORMATION_CHANGED_MESSAGE)
+        assert info.text == 'Информация успешно сохранена'
+
+    @pytest.mark.UI
+    @pytest.mark.parametrize('button, page_attr, url',
+                             [(basic_locators.PROFILE_BUTTON, basic_locators.PROFILE_ATTR, '/profile'),
+                              (basic_locators.BALANCE_BUTTON, basic_locators.BALANCE_ATTR, '/billing')])
+    def test_header_buttons(self, button, page_attr, url):
+        self.login(login=self.login_value, password=self.pass_value)
+        self.click_button(button)
+        assert self.driver.current_url.__contains__(url)
+        assert self.is_present(page_attr) == True
