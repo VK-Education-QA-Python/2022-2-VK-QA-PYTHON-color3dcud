b4d8570a80ae1dfec5b8558b8f7f269426996894
Danil Vorobev <dvorobiev@agentapp.ru>
2022-10-14 23:10:02 +0300

    Были изменены проверки:  1. В test_login ожидаемый элемент был изменен на HEADER_BUTTON  2. В test_logout ожидаемый элемент был изменен на HEADER_LOGIN_BUTTON, был убран assert на URL  3. В test_negative_invalid_login_or_pass было убрано множество assert-ов, ожидаемый элемент был изменен на LOGIN_FORM_MESSAGE  4. В test_change_profile_data была убрана проверка текста информационного сообщения  5. В test_header_buttons был убран assert на URL  6. Во всех тестах self.is_present(locator) больше не сравнивается с True

--- homework1/test_hw1.py
+++ homework1/test_hw1.py
@@ -13,30 +13,26 @@ class TestOne(BaseCase):
     @pytest.mark.UI
     def test_login(self):
         self.login(login=self.login_value, password=self.pass_value)
-        assert self.find(basic_locators.USERNAME).text == self.login_value.upper()
+        assert self.is_present(basic_locators.HEADER_BUTTON)
 
     @pytest.mark.UI
     def test_logout(self):
         self.login(login=self.login_value, password=self.pass_value)
-        assert self.find(basic_locators.USERNAME).text == self.login_value.upper()
+        assert self.is_present(basic_locators.HEADER_BUTTON)
         self.driver.refresh()
         self.click_button(basic_locators.HEADER_BUTTON)
         self.click_button(basic_locators.LOGOUT_BUTTON)
-        assert self.is_present(basic_locators.MAIN_PAGE_DIV) == True
-        assert self.driver.current_url == 'https://target-sandbox.my.com/'
+        assert self.is_present(basic_locators.HEADER_LOGIN_BUTTON)
 
     @pytest.mark.UI
     def test_negative_invalid_login_or_pass(self):
         self.login(login='111', password='111')
-        assert self.is_present(basic_locators.LOGIN_FORM_MESSAGE_TITLE) == True
-        assert self.is_present(basic_locators.LOGIN_FORM_MESSAGE_TEXT) == True
-        assert self.find(basic_locators.LOGIN_FORM_MESSAGE_TITLE).text == 'Error'
-        assert self.find(basic_locators.LOGIN_FORM_MESSAGE_TEXT).text == 'Invalid login or password'
+        assert self.is_present(basic_locators.LOGIN_FORM_MESSAGE)
 
     @pytest.mark.UI
     def test_negative_login_with_russian_credentials(self):
         self.login(login='почта', password='пароль')
-        assert self.is_present(basic_locators.ENTER_EMAIL_OR_PHONE_ERROR) == True
+        assert self.is_present(basic_locators.ENTER_EMAIL_OR_PHONE_ERROR)
 
     @pytest.mark.UI
     def test_change_profile_data(self):
@@ -47,16 +43,13 @@ class TestOne(BaseCase):
         new_phone = ('+7999' + str(random.randint(1000000, 9999999)))
         self.change_profile_field(basic_locators.PHONE_FIELD, new_phone)
         self.click_button(basic_locators.SAVE_PROFILE_BUTTON)
-        assert self.is_present(basic_locators.INFORMATION_CHANGED_MESSAGE) == True
-        info = self.find(basic_locators.INFORMATION_CHANGED_MESSAGE)
-        assert info.text == 'Информация успешно сохранена'
+        assert self.is_present(basic_locators.INFORMATION_CHANGED_MESSAGE)
 
     @pytest.mark.UI
-    @pytest.mark.parametrize('button, page_attr, url',
-                             [(basic_locators.PROFILE_BUTTON, basic_locators.PROFILE_ATTR, '/profile'),
-                              (basic_locators.BALANCE_BUTTON, basic_locators.BALANCE_ATTR, '/billing')])
-    def test_header_buttons(self, button, page_attr, url):
+    @pytest.mark.parametrize('button, page_attr',
+                             [(basic_locators.PROFILE_BUTTON, basic_locators.PROFILE_ATTR),
+                              (basic_locators.BALANCE_BUTTON, basic_locators.BALANCE_ATTR)])
+    def test_header_buttons(self, button, page_attr):
         self.login(login=self.login_value, password=self.pass_value)
         self.click_button(button)
-        assert self.driver.current_url.__contains__(url)
-        assert self.is_present(page_attr) == True
+        assert self.is_present(page_attr)
