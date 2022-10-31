afe60716ddc3dba4afddf3615a0b9fa91401f228
Danil Vorobev <dvorobiev@agentapp.ru>
2022-10-31 23:48:35 +0300

    В тесты добавлены потерянные ассерты

--- homework3/test_api.py
+++ homework3/test_api.py
@@ -13,9 +13,9 @@ class TestSegments(ApiBase):
     @pytest.mark.API
     def test_games_segment_creation(self, new_games_segment):
         games_segment = new_games_segment
-        self.check_segment_in_segments_list(segment_id=games_segment.segment_id)
+        assert self.check_segment_in_segments_list(segment_id=games_segment.segment_id)
 
     @pytest.mark.API
     def test_vk_segment_creation(self, new_vk_segment):
         vk_segment = new_vk_segment
-        self.check_segment_in_segments_list(segment_id=vk_segment.segment_id)
+        assert self.check_segment_in_segments_list(segment_id=vk_segment.segment_id)
